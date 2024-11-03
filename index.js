094ecee9bede635db9f9e5d9118d2b263faa5047
David Pamplona <davypamp@gmail.com>
2024-11-03 16:15:26 -0300

    Adicionando o carrosel ao projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.box-carrosel').slick({
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2400,
+      arrows: true,
+    });
+  });
+
